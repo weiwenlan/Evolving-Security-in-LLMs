f1664880ce4ab4660f749b373e08f5c7f6099383
WEIWENLAN <weiwenlan1999@gmail.com>
2024-11-27 14:08:11 -0600

    CHECKED CipherChat

---
+++ attack/CipherChatWenlan/wenlan_run.sh
@@ -0,0 +1,7 @@
+python3 wenlan_main.py \
+ --model_name gpt-3.5-turbo \
+--data_path data/data_en_zh.dict \
+--encode_method caesar \
+--instruction_type Crimes_And_Illegal_Activities \
+--demonstration_toxicity toxic \
+--language en
