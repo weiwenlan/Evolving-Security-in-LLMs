0f4ef450cf15de9f180875389f336001df580488
Austin Shang <austins@Austins-MBP.attlocal.net>
2024-12-03 21:28:52 -0800

    fix llama guard for gptfuzz

--- defenses/llama_guard/get_attack_table.py
+++ defenses/llama_guard/get_attack_table.py
@@ -14,7 +14,7 @@ class AttackDatabase:
         """
         connection = sqlite3.connect(self.db_path)
         cursor = connection.cursor()
-        table_name = "Attacks"
+        table_name = "chat_responses"
 
         try:
             # check whether the table exists
@@ -32,10 +32,10 @@ class AttackDatabase:
     
     def get_all_attacks(self):
         # query all the attacks 
-        query = "SELECT * FROM Attacks"
+        query = "SELECT * FROM chat_responses"
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
-        return [{"id": row[0], "paper_name": row[1], "attack_category": row[2], "attack_prompt": row[3]} for row in rows]
+        return [{"id": row[0], "prompt": row[1], "response": row[2], "parent": row[3], "result": row[4]} for row in rows]
 
     def close(self):
         self.connection.close()
