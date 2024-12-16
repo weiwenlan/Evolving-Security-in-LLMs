2e361b1320ef11478bf4b5138eff95954e86332b
Austin Shang <austins@Austins-MBP.attlocal.net>
2024-12-15 22:34:47 -0800

    add gpt series api and fix some error

--- defenses/run_defense.sh
+++ defenses/run_defense.sh
@@ -1,26 +1,17 @@
-#!/bin/bash
-
-# # 第一次运行，指定第一个 db_path 和输出文件
-# nohup python defenses.py --defense_method smooth_llm --db_path "/Users/austins/Adversarial-Attacks-on-LLM/data/500/jailbroken_llama3.1_8b_500_chat_requests.db" > jailbroken.txt 2>&1 &
-
-# # 第二次运行，指定第二个 db_path 和输出文件
-# nohup python defenses.py --defense_method smooth_llm --db_path "/Users/austins/Adversarial-Attacks-on-LLM/data/500/reNeLLM_llama3.1_8b_500_chat_requests.db" --model > renellm.txt 2>&1 &
-
-#!/bin/bash
-
-methods=("smooth_llm")
+methods=("smooth_llm", "goal_prioritization", "llama_guard") 
 
 db_paths=(
     "/Users/austins/Adversarial-Attacks-on-LLM/data/500/jailbroken_llama3.1_8b_500_chat_requests.db"
-    "/Users/austins/Adversarial-Attacks-on-LLM/data/500/reNeLLM_llama3.1_8b_500_chat_requests.db"
 )
+
+target_model='llama31_70b'
 
 for method in "${methods[@]}"; do
   echo "Starting method: $method"
   for db_path in "${db_paths[@]}"; do
-    db_name=$(basename "$db_path" .db)
+    db_name=$(basename "$db_path")
     output_file="${method}_${db_name}.txt"
-    nohup python defenses.py --defense_method "$method" --db_path "$db_path" > "$output_file" 2>&1 &
+    nohup python defenses.py --defense_method "$method" --db_path "$db_path" --target_model "$target_model" > "$output_file" 2>&1 &
     echo "Started $method on $db_path, output saved to $output_file"
   done
   wait
