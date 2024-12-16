876ccae9a45a3fa53ab51966fb6d4fbe566b8393
Austin Shang <austins@Austins-MBP.attlocal.net>
2024-12-15 23:02:27 -0800

    fix defense shell error

--- defenses/run_defense.sh
+++ defenses/run_defense.sh
@@ -1,16 +1,24 @@
-methods=("smooth_llm", "goal_prioritization", "llama_guard") 
+methods=("llama_guard" "goal_prioritization") 
 
 db_paths=(
-    "/Users/austins/Adversarial-Attacks-on-LLM/data/500/jailbroken_llama3.1_8b_500_chat_requests.db"
+    "/Users/austins/Adversarial-Attacks-on-LLM/data/500/llama31-70b/cipherChat_llama3.1_70b_chat_requests_500.db"
+    "/Users/austins/Adversarial-Attacks-on-LLM/data/500/llama31-70b/GPTFuzz_llama3.1_70b_chat_requests_500.db"
+    "/Users/austins/Adversarial-Attacks-on-LLM/data/500/llama31-70b/jailbroken_llama_3.1_70b_chat_requests_500.db"
+    "/Users/austins/Adversarial-Attacks-on-LLM/data/500/llama31-70b/reNeLLM_llama3.1_70b_chat_requests_500.db"
 )
 
-target_model='llama31_70b'
+# db_paths=(
+#       "/Users/austins/Adversarial-Attacks-on-LLM/data/500/gpt-3.5-turbo/cipherChat_gpt3.5_chat_requests_500.db"
+# )
+
+# target_model='gpt-3.5-turbo'
+target_model='llama31-70b'
 
 for method in "${methods[@]}"; do
   echo "Starting method: $method"
   for db_path in "${db_paths[@]}"; do
     db_name=$(basename "$db_path")
-    output_file="${method}_${db_name}.txt"
+    output_file="${method}_${db_name}_${target_model}.txt"
     nohup python defenses.py --defense_method "$method" --db_path "$db_path" --target_model "$target_model" > "$output_file" 2>&1 &
     echo "Started $method on $db_path, output saved to $output_file"
   done
