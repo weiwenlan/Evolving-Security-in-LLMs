38d6b5c5cf30e4b16df8957d29bdae0325587b21
Austin Shang <austins@Austins-MBP.attlocal.net>
2024-12-15 22:34:47 -0800

    add gpt series api and fix some error

--- defenses/helpers/model_configs.py
+++ defenses/helpers/model_configs.py
@@ -7,5 +7,11 @@ MODELS = {
     },
     'llama-guard-3-8b':{
         'model_path': 'meta-llama/Llama-Guard-3-8B'
+    },
+    'gpt-3.5-turbo':{
+        'model_path': 'gpt-3.5-turbo'        
+    },
+    'gpt-4-turbo':{
+        'model_path': 'gpt-4-turbo'
     }
 }
