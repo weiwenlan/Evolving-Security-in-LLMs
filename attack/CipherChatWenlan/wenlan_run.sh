34e26926deb1e90dc35c6d928efb2378eb127dd8
WEIWENLAN <weiwenlan1999@gmail.com>
2025-01-23 17:10:59 -0600

    Other change for mistral

--- attack/CipherChatWenlan/wenlan_run.sh
+++ attack/CipherChatWenlan/wenlan_run.sh
@@ -1,12 +1,30 @@
+#!/bin/bash
 
-for i in "Ethics_And_Morality" \
-         "Inquiry_With_Unsafe_Opinion" "Insult" "Mental_Health" "Physical_Harm" \
-         "Role_Play_Instruction" \
-         "Unfairness_And_Discrimination" 
-do
+# Check if the model_name argument is provided
+if [ "$#" -ne 2 ] || [ "$1" != "--model_name" ]; then
+    echo "Usage: sh wenlan_run.sh --model_name <model_name>"
+    exit 1
+fi
+
+# Extract the model_name from arguments
+MODEL_NAME=$2
+
+# List of instruction types
+INSTRUCTION_TYPES=(
+    "Ethics_And_Morality"
+    "Inquiry_With_Unsafe_Opinion"
+    "Insult"
+    "Mental_Health"
+    "Physical_Harm"
+    "Role_Play_Instruction"
+    "Unfairness_And_Discrimination"
+)
+
+# Loop through instruction types and run the Python script
+for INSTRUCTION_TYPE in "${INSTRUCTION_TYPES[@]}"; do
     python wenlan_main.py \
-        --model_name vicuna-7b-v1.1 \
+        --model_name "$MODEL_NAME" \
         --encode_method unchange \
-        --instruction_type $i \
+        --instruction_type "$INSTRUCTION_TYPE" \
         --debug_num 40
 done
--- attack/GPTFuzzWenlan/gptfuzzer/llm/__init__.py
+++ attack/GPTFuzzWenlan/gptfuzzer/llm/__init__.py
@@ -1,1 +1,1 @@
-from .llm import LLM, OpenAILLM, ClaudeLLM, LlamaLLM, VertexLLM, VertexHuggingFaceLLM
+from .llm import LLM, OpenAILLM, ClaudeLLM, LlamaLLM, VertexLLM, VertexHuggingFaceLLM, MistralLLM
