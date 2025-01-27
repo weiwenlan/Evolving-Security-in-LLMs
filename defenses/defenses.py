f4d9ea7aaca0a5e4bd9dee387c9aeb3421206ef0
Austin Shang <austins@Austins-MBP.attlocal.net>
2025-01-27 14:18:19 -0800

    finish llama2 7b and 2 70b

--- defenses/defenses.py
+++ defenses/defenses.py
@@ -24,22 +24,23 @@ if __name__ == "__main__":
     parser.add_argument(
         '--defense_method', 
         type = str,
-        default = 'null',
+        
+        default = 'smooth_llm',
         choices = ['goal_prioritization', 'llama_guard', 'smooth_llm'],
         help = 'Specify the defense method to run with.'
     )
     parser.add_argument(
         '--target_model',
         type=str,
-        default='null',
+        default='llama2-70b-hf',
         choices=['llama31-8b', 'llama31-70b', 'gpt-3.5-turbo', 'gpt-4-turbo', 'vicuna7b15', 'vicuna13b15', 'vicuna7b11', 'vicuna13b11', 'llama2-70b-hf', 'llama2-7b-hf', 'mistral7b-01', 'mistral7b-02', 'mistral7b-03', 'mistral12b-nemo'],
         help='Specify the target model to run with.'
     )
 
     parser.add_argument(
-        "--db_path", 
+        "--db_path",
         type=str,
-        default="/Users/austins/Adversarial-Attacks-on-LLM/data/500/evaluated/llama2-70b/jailbroken_llama2_70b_chat_requests_500.db",
+        default="/Users/austins/Adversarial-Attacks-on-LLM/data/500/evaluated/llama2-70b/austin.db",
         help="Path to the experiments database."
     )
 
