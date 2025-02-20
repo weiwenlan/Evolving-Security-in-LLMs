b9842ac27ca12b7c65320f45b235fab7d1bfe9ed
Austin Shang <austins@Austins-MBP.attlocal.net>
2025-02-19 16:16:53 -0800

    improve evaluation pipeline

--- defenses/helpers/model_configs.py
+++ defenses/helpers/model_configs.py
@@ -1,6 +1,6 @@
 MODELS = {
     'llama31-8b': {
-        'model_path': 'meta-llama/Meta-Llama-3.1-8B-Instruct'
+        'model_path': 'meta-llama/Meta-Llama-3.1-8B-Instruct' # this is old api path, please update new path 
     },
     'llama31-70b':{
         'model_path': 'meta-llama/Meta-Llama-3.1-70B-Instruct'
